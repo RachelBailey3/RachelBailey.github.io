73dc17943f70b7ca670fab1e6f9b2edecde2cd4e
RachelBailey3 <140627033+RachelBailey3@users.noreply.github.com>
2023-07-28 20:08:56 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const collapsibleBtn = document.querySelector(".collapsible-btn");
-  const collapsibleContent = document.querySelector(".collapsible-content");
+      const collapsibleBtn = document.querySelector(".collapsible-btn");
+      const collapsibleContent = document.querySelector(".collapsible-content");
 
-  collapsibleBtn.addEventListener("click", function () {
-    if (collapsibleContent.style.display === "none") {
-      collapsibleContent.style.display = "block";
-    } else {
-      collapsibleContent.style.display = "none";
-    }
-  });
-});
+      collapsibleBtn.addEventListener("click", function () {
+        if (collapsibleContent.style.display === "none") {
+          collapsibleContent.style.display = "block";
+        } else {
+          collapsibleContent.style.display = "none";
+        }
+      });
+    });
