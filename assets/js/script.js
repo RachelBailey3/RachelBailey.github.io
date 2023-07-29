669047e148eec8c49ea57d1ef1e4654a79fb08e0
RachelBailey3 <140627033+RachelBailey3@users.noreply.github.com>
2023-07-28 20:06:26 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-function showText(textId) {
-  const texts = document.querySelectorAll('#textContainer p');
-  texts.forEach((text) => {
-    text.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  const collapsibleBtn = document.querySelector(".collapsible-btn");
+  const collapsibleContent = document.querySelector(".collapsible-content");
+
+  collapsibleBtn.addEventListener("click", function () {
+    if (collapsibleContent.style.display === "none") {
+      collapsibleContent.style.display = "block";
+    } else {
+      collapsibleContent.style.display = "none";
+    }
   });
-  const selectedText = document.getElementById(textId);
-  if (selectedText) {
-    selectedText.style.display = 'block';
-  }
-}
+});
